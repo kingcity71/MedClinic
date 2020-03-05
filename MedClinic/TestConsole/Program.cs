ed5d58f9f3037aff4f5d4be58a307f1070a3c405
Dmitry <kingcity71@ya.ru>
2020-03-05 20:17:27 +0300

    test console added

---
+++ MedClinic/TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MedClinic.Data;
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new MedClinicContext())
+            {
+                var q = context.Patients;
+            }
+        }
+    }
+}
